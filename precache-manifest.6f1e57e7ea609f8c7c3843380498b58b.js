0d667095f4c1604ad7a139c1af5607b5397c019a
{psw9058) <s05150732w@gmail.com>
2022-04-13 15:33:22 +0900

    Updates

---
+++ precache-manifest.6f1e57e7ea609f8c7c3843380498b58b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "596db0b1383be03fd36ad13a0e44024d",
+    "url": "/card-maker/index.html"
+  },
+  {
+    "revision": "256ef8cf8897fdcf85e7",
+    "url": "/card-maker/static/css/main.e012e617.chunk.css"
+  },
+  {
+    "revision": "08984b362455d1492c4c",
+    "url": "/card-maker/static/js/2.d2bc21af.chunk.js"
+  },
+  {
+    "revision": "7d2282234cd0247bd4712fb2834fc8d9",
+    "url": "/card-maker/static/js/2.d2bc21af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "256ef8cf8897fdcf85e7",
+    "url": "/card-maker/static/js/main.7ab44067.chunk.js"
+  },
+  {
+    "revision": "7100c9d9ce85d7d659c7",
+    "url": "/card-maker/static/js/runtime-main.6126bd76.js"
+  }
+]);
